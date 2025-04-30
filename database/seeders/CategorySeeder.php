398ed80b914ab8d4d8c7ebff93789942bb77beb2
Michael Hampton <michaelhamptondesign@yahoo.com>
2025-04-30 14:08:55 +0100

    update seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\CategoryAttributes;
+use App\Models\ProductAttribute;
 use Faker\Generator;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
@@ -16,18 +18,107 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $faker = app(Generator::class);
+        $productAttributes = ProductAttribute::all();
 
-        $categories = [
-            ['parent_id' => 0, 'name' => 'Clothing', 'slug' => 'clothing', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
-            ['parent_id' => 0, 'name' => 'Electronics', 'slug' => 'electronics', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
-            ['parent_id' => 0, 'name' => 'Appliances', 'slug' => 'appliances', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
-            ['parent_id' => 1, 'name' => 'Men', 'slug' => 'men', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
-            ['parent_id' => 1, 'name' => 'Women', 'slug' => 'women', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
-            ['parent_id' => 1, 'name' => 'Kids', 'slug' => 'kids', 'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()), 'is_featured' => 1, 'menu_status' => 1, 'active' => 1, 'description' => $faker->sentence(), 'meta_title' => $faker->sentence(), 'meta_keywords' => $faker->word()],
+        $categoryAttributes = [
+            'Clothing' => ['Condition', 'Color', 'Size'], 
+            'Electronics' => ['Color', 'Condition'], 
+            'Appliances' => ['Color', 'Condition'], 
+            'Men' => ['Condition', 'Color', 'Size'], 
+            'Women' => ['Condition', 'Color', 'Size'], 
+            'Kids' => ['Condition', 'Color', 'Size']
         ];
 
+        $categories = [
+            [
+                'parent_id' => 0,
+                'name' => 'Clothing',
+                'slug' => 'clothing',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+            [
+                'parent_id' => 0,
+                'name' => 'Electronics',
+                'slug' => 'electronics',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+            [
+                'parent_id' => 0,
+                'name' => 'Appliances',
+                'slug' => 'appliances',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+            [
+                'parent_id' => 1,
+                'name' => 'Men',
+                'slug' => 'men',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+            [
+                'parent_id' => 1,
+                'name' => 'Women',
+                'slug' => 'women',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+            [
+                'parent_id' => 1,
+                'name' => 'Kids',
+                'slug' => 'kids',
+                'image' => Str::replace('categories/', '', collect(Storage::disk('public')->files('categories'))->random()),
+                'is_featured' => 1,
+                'menu_status' => 1,
+                'active' => 1,
+                'description' => $faker->sentence(),
+                'meta_title' => $faker->sentence(),
+                'meta_keywords' => $faker->word()
+            ],
+        ];
+
+
         foreach ($categories as $category) {
-            Category::create($category);
+            $category = Category::create($category);
+
+            if(array_key_exists($category['name'], $categoryAttributes)) {
+                foreach($categoryAttributes[$category['name']] as $key => $value) {
+                    $attribute = $productAttributes->where('name', $value)->first()->id;
+
+                    CategoryAttributes::create([
+                        'category_id' => $category->id,
+                        'attribute_id' => $attribute,
+                        'active' => true
+                    ]);
+                }
+            }
         }
     }
 }
---
+++ database/seeders/DeliveryMethodSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Courier;
+use App\Models\DeliveryMethod;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DeliveryMethodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $couriers = Courier::all();
+
+        $countryIds = [243, 244, 245, 286, 287, 290, 297, 342, 412, 468];
+        $sizes = ['Large' => 3.99, 'Medium' => 2.88, 'Small' => 1.99];
+
+        foreach($countryIds as $country) {
+            foreach($sizes as $key => $size) {
+               DeliveryMethod::create(['name' => $key, 'price' => $size, 'country_id' => $country, 'courier_id' => $couriers->first()->id]);
+            }
+        }
+    }
+}
