28c67b687dafd3530a5fcf4c0592dc44f3a66bac
Michael Hampton <michaelhamptondesign@yahoo.com>
2025-05-17 15:46:40 +0100

    payment gateway part 1

--- resources/frontend/admin/src/environments/environment.development.ts
+++ resources/frontend/admin/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   apiUrl: 'http://127.0.0.1:8000/api',
+  stripeKey: 'pk_test_51R9Vf8IX0IyYlljO5UDnTbsBgLs55Qga8219HImZozVLMoQrNDtETPiZD3BGPMrGp9ZKzfOmUV3eImXMT1bQogXD00fZ3wpIgf',
+  stripeSecret: 'sk_test_51R9Vf8IX0IyYlljO7EBgh753scCaPNh3iOeewqnEoSF4663Sl3XPktfavP9vtEdx1hNtYF75XLE4iPwQQNd6QNJe008OUfLsMZ'
 
 };
